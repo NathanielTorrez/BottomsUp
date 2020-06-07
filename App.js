4fdce37800149c96c7c3385f8c34f4c69b2732cc
Nathaniel Torrez <mr.realestatenate@gmail.com>
2020-06-06 17:30:52 -0700

    refactored to use state

--- App.js
+++ App.js
@@ -3,37 +3,46 @@ import { StyleSheet, Text, View, Button, TextInput, Picker } from 'react-native'
 
 import tailwind from 'tailwind-rn';
 
-export default function App() {
-  const page = useState('home');
-  const alcohol = useState('');
-  const juice = useState('');
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <View style={tailwind('bg-gray-800')}>
-      <Text style={tailwind('mt-6 text-center text-3xl text-yellow-500')}>Bottoms Up!</Text>
-      <Text style={tailwind('mt-6 text-center text-xl text-yellow-500')}>Pick your sauce</Text>
-      <Picker>
-        <Picker.item label="Whiskey" value="Whiskey" />
-        <Picker.item label="Rum" value="Rum" />
-        <Picker.item label="Vodka" value="Vodka" />
-        <Picker.item label="Tequilla" value="Tequilla" />
-        <Picker.item label="Gin" value="Gin" />
-      </Picker>
-      <Text style={tailwind('mt-6 text-center text-xl text-yellow-500')}>Pick your juice</Text>
-      <Picker>
-        <Picker.item label="Pineapple" value="Pineapple" />
-        <Picker.item label="Orange" value="Orange" />
-        <Picker.item label="Triple Sec" value="Triple Sec" />
-        <Picker.item label="Bitters" value="Bitters" />
-        <Picker.item label="Simple Syrup" value="Simple Syrup" />
-      </Picker>
-      <Button
-        color="#ecc94b"
-        title="show me the drinks"
-        onPress={() => {
-          setPage('results');
-        }}
-      />
-    </View>
-  );
+    this.state = {
+      page: '',
+    };
+    this.changePage = this.changePage.bind(this);
+  }
+
+  changePage = (e) => {
+    e.preventDefault();
+    this.setState({
+      page: e.tar,
+    });
+  };
+
+  render() {
+    return (
+      <View style={tailwind('bg-gray-800')}>
+        <Text style={tailwind('mt-6 text-center text-3xl text-yellow-500')}>Bottoms Up!</Text>
+        <Text style={tailwind('mt-6 text-center text-xl text-yellow-500')}>Pick your sauce</Text>
+        <Picker>
+          <Picker.item label="Whiskey" value="Whiskey" />
+          <Picker.item label="Rum" value="Rum" />
+          <Picker.item label="Vodka" value="Vodka" />
+          <Picker.item label="Tequilla" value="Tequilla" />
+          <Picker.item label="Gin" value="Gin" />
+        </Picker>
+        <Text style={tailwind('mt-6 text-center text-xl text-yellow-500')}>Pick your juice</Text>
+        <Picker>
+          <Picker.item label="Pineapple" value="Pineapple" />
+          <Picker.item label="Orange" value="Orange" />
+          <Picker.item label="Triple Sec" value="Triple Sec" />
+          <Picker.item label="Bitters" value="Bitters" />
+          <Picker.item label="Simple Syrup" value="Simple Syrup" />
+        </Picker>
+        <Button color="#ecc94b" title="show me the drinks" onPress={this.changePage} />
+      </View>
+    );
+  }
 }
+export default App;
